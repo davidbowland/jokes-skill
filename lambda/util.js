be57a047b0a075ee111e84d0b6d2e8a727d8d5b9
David Bowland <dbowland1@gmail.com>
2022-03-26 13:05:34 -0500

    Sort imports, keys, and vars

--- lambda/util.js
+++ lambda/util.js
@@ -8,8 +8,8 @@ module.exports.getS3PreSignedUrl = function getS3PreSignedUrl(s3ObjectKey) {
   const bucketName = process.env.S3_PERSISTENCE_BUCKET
   const s3PreSignedUrl = s3SigV4Client.getSignedUrl('getObject', {
     Bucket: bucketName,
-    Key: s3ObjectKey,
     Expires: 60 * 1, // the Expires is capped for 1 minute
+    Key: s3ObjectKey,
   })
   console.log(`Util.s3PreSignedUrl: ${s3ObjectKey} URL ${s3PreSignedUrl}`)
   return s3PreSignedUrl
